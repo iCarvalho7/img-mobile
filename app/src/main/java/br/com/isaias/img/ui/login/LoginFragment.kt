99c62e6dfa4edb3c2fcf67c0523221b5bc5db19b
Isaías Carvalho <isaias.carvalho@itbam.org.br>
2021-02-23 19:56:10 -0400

    created login screen

---
+++ app/src/main/java/br/com/isaias/img/ui/login/LoginFragment.kt
@@ -0,0 +1,30 @@
+package br.com.isaias.img.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.isaias.img.R
+import br.com.isaias.img.databinding.FragmentLoginBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment()] to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment using biding
+        return FragmentLoginBinding.inflate(inflater, container, false).root
+    }
+}
